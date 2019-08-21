9b28c91f1b7c09b8db24de8ea98c2a2376c80001
wpf9264 <820697479@qq.com>
2019-08-21 16:53:05 +0800

    03-printListFromTailToHead.cpp

---
+++ 03-printListFromTailToHead.cpp
@@ -0,0 +1,21 @@
+/*从头到尾打印链表*/
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
+class Solution {
+public:
+    vector<int> res;
+    vector<int> printListFromTailToHead(ListNode* head) {
+        if (head!=nullptr){
+            printListFromTailToHead(head->next);
+            res.push_back(head->val);
+        }
+        return res;
+    }
+};
